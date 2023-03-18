24810a5902cbed2079bbf4d56d159ecc76a3ca56
example2023 <127644888+example2023@users.noreply.github.com>
2023-03-18 21:32:50 +0500

    Add files via upload

---
+++ pgzero_test.py
@@ -0,0 +1,19 @@
+import pgzrun
+
+WIDTH = 600 # Ширина окна
+HEIGHT = 300 # Высота окна
+
+TITLE = "Инопланетный раннер" # Заголовок окна игры
+FPS = 30 # Количество кадров в секунду
+
+alien = Actor('alien', (50, 240))
+fon = Actor("fon")
+
+def draw():
+    fon.draw()
+    alien.draw()
+    
+def update(dt):
+    alien.x = alien.x + 5
+    
+pgzrun.go()
